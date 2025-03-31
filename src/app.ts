cb1dd8867dd052e6bb5ccfc804be961d23034453
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-03-31 11:25:21 +0530

    remove the unnecessary console.

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,5 @@ app.get("/", (_req, res) => {
 });
 
 app.use("/slack", slackRoutes);
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/services/slackService.ts
+++ src/services/slackService.ts
@@ -2,5 +2,4 @@
 import { WebClient } from "@slack/web-api";
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.SLACK_BOT_TOKEN);
 export const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
