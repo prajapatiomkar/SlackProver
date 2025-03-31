80fa47482aad469ff219435f465a89019ad12ea4
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-03-31 11:19:24 +0530

    added check route.

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,12 @@ import dotenv from "dotenv";
 import slackRoutes from "./routes/slackRoutes";
 
 dotenv.config();
+
 const app = express();
+app.get("/", (_req, res) => {
+  res.send("SlackProver backend is running!");
+});
+
 app.use("/slack", slackRoutes);
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
